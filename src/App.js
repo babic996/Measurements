5e5829c6a72486b7a348d81f2eafe0ccf5ec2392
Harun Hadzic <48292553+hhadzic3@users.noreply.github.com>
2022-11-23 11:26:00 +0100

    React router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 import Header from './components/Header/Header';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import routes from './routes';
 
 function App() {
   return (
-    <>
+    <Router>
       <head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" /></head>
       <Header />
-    </>
+      <div className="App">
+        <h1>Welcome</h1>
+        <Routes>
+            {routes.map((route, i) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.component}
+              />
+            ))}
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
