ce653006f2e21d7f29f797deaf4941c5e857f272
Aleksandar Babić <aleksandar.babic996@gmail.com>
2022-11-23 14:30:23 +0100

    add Header and Footer

--- src/App.js
+++ src/App.js
@@ -6,23 +6,24 @@ import {
   Routes,
 } from "react-router-dom";
 import routes from './routes';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <Router>
       <head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" /></head>
-      <Header />
-      <div className="App">
-        <h1>Welcome</h1>
-        <Routes>
-            {routes.map((route, i) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={route.component}
-              />
-            ))}
-          </Routes>
+      <div className="main">
+        <Header />
+          <Routes>
+              {routes.map((route, i) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={route.component}
+                />
+              ))}
+            </Routes>
+        <Footer />
       </div>
     </Router>
   );
