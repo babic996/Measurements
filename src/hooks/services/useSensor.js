4e48601488f9b2d30f46b01d664a641df16a707e
Aleksandar Babić <aleksandar.babic996@gmail.com>
2022-11-30 09:22:52 +0100

    edited useSensor

--- src/hooks/services/useSensor.js
+++ src/hooks/services/useSensor.js
@@ -1,41 +1,33 @@
 import { Request } from '../../util/useAxios'
 
-export const useSensorLastChange = (id) => {
-    return (
-        async () => getSensorLastChange(id)
-    );
-}
 
-export const useSensorLast10Changes = (id) => {
-    return (
-        async () => getSensorLast10Change(id)
-    );
-}
-
-export const useAllSensors = () => {
-    return (
-        async () => getAllSensors()
-    );
-}
-
-
-const getSensorLastChange = async (id) => {
+export const getSensorLastChange = async (id) => {
     const request = Request();
 
     const result = await request({ url: `/sensor/last-change?sensorId=${id}`, method: 'get' });
     return result.data;
 }
 
-const getAllSensors = async () => {
+export const getAllSensors = async () => {
     const request = Request();
 
     const result = await request({ url: `/sensor/find-all`, method: 'get' });
     return result.data;
 }
 
-const getSensorLast10Change = async (id) => {
+export const getSensorLast10Change = async (id) => {
     const request = Request();
 
     const result = await request({ url: `/sensor/last-10-changes?sensorId=${id}`, method: 'get' });
     return result.data;
 }
+
+export const getSensor = async (id) => {
+    const request = Request();
+
+    const result = await request({
+      url: `/sensor?sensorId=${id}`,
+      method: "get",
+    });
+    return result.data;
+  };
--- src/pages/Sensor.jsx
+++ src/pages/Sensor.jsx
@@ -3,8 +3,11 @@ import { Col, Row } from "antd";
 import Card from "../components/Card/Card";
 import MeasurementTable from "../components/Table/Table";
 import { useParams } from "react-router";
-import { Request } from "../util/useAxios";
 import moment from "moment";
+import { getSensorLastChange } from "../hooks/services/useSensor";
+import { getSensorLast10Change } from "../hooks/services/useSensor";
+import { getAllSensors } from "../hooks/services/useSensor";
+import { getSensor } from "../hooks/services/useSensor";
 
 const Sensor = () => {
   const { sensorId } = useParams();
@@ -36,44 +39,7 @@ const Sensor = () => {
         });
       });
     }
-  }, [sensorId]);
-
-  const getSensorLastChange = async (id) => {
-    const request = Request();
-
-    const result = await request({
-      url: `/sensor/last-change?sensorId=${id}`,
-      method: "get",
-    });
-    return result.data;
-  };
-
-  const getSensor = async (id) => {
-    const request = Request();
-
-    const result = await request({
-      url: `/sensor?sensorId=${id}`,
-      method: "get",
-    });
-    return result.data;
-  };
-
-  const getSensorLast10Change = async (id) => {
-    const request = Request();
-
-    const result = await request({
-      url: `/sensor/last-10-changes?sensorId=${id}`,
-      method: "get",
-    });
-    return result.data;
-  };
-
-  const getAllSensors = async () => {
-    const request = Request();
-
-    const result = await request({ url: `/sensor/find-all`, method: "get" });
-    return result.data;
-  };
+  }, [sensorId])
 
   return (
     <>
